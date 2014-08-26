f0f4e326d5ae051559a37b74c77acb5a4c259745
Eric Brelsford <ebrelsford@gmail.com>
2014-08-26 16:47:12 -0400

    Get more media, check whether it's already added
    Pagination, currently 10 pages of 20 media at a time. Check CartoDB for the
    largest id of media already included, only add things with larger ids.

--- python/harvester.py
+++ python/harvester.py
@@ -7,10 +7,10 @@ from instagram.client import InstagramAPI
 
 api_key = '700987471291d9c006a998b500f23de243f7388a'
 cartodb_domain = 'jossphilippe'
+cl = CartoDBAPIKey(api_key, cartodb_domain)
 
 
 def insert_into_cartodb(sql_query):
-    cl = CartoDBAPIKey(api_key, cartodb_domain)
     try:
        # your CartoDB account:
         print cl.sql(sql_query)
@@ -18,9 +18,23 @@ def insert_into_cartodb(sql_query):
         print ("some error ocurred", e)
 
 
-def Instagram_API():
+def get_max_id():
+    return cl.sql('SELECT MAX(media_id) FROM instagram_media')['rows'][0]['max']
+
+
+def Instagram_API(min_id=None):
     api = InstagramAPI(client_id='edf88a2a6fa94c9889e9800db8317f68', client_secret='73975e1a5b674c8e961568c463f3a8ec')
-    bagitnyc_media, pagination = api.tag_recent_media(150, 0, "bagitnyc")
+    bagitnyc_media = []
+
+    # Get up to 200 media
+    for page in api.tag_recent_media(tag_name='bagitnyc', as_generator=True,
+                                     max_pages=10):
+        # Open the page and push its contents onto bagitnyc_media
+        bagitnyc_media += page[0]
+
+    # If min_id passed, only return media that came after it
+    if min_id:
+        bagitnyc_media = filter(lambda m: m.id > min_id, bagitnyc_media)
     return bagitnyc_media
 
 
@@ -43,26 +57,10 @@ if __name__ == '__main__':
     # For each row in whatever is returned from get_data(), create an SQL
     # INSERT statement for that data, then insert it into CartoDB.
     #
-    for media in Instagram_API():
+    for media in Instagram_API(min_id=get_max_id()):
         try:
-            # 4/8, EB: Added this check to ONLY look at pieces of media with a
-            # location
+            # ONLY look at pieces of media with a location
             if media.location:
-                # 4/8, EB: Changed this line to print lat and lng out
-                print media.images['standard_resolution'].url, media.user.username, media.id, media.location.point.latitude, media.location.point.longitude, media.created_time
-
-                # 4/8, EB: Added this line to print SQL statements for each
-                # piece of media
-                print create_sql_statement(
-                    media.location.point.longitude,
-                    media.location.point.latitude,
-                    media.images['standard_resolution'].url,
-                    media.user.username,
-                    media.id,
-                    media.link,
-                    media.created_time
-                )
-                
                 sql_query = create_sql_statement(
                     media.location.point.longitude,
                     media.location.point.latitude,
@@ -72,12 +70,9 @@ if __name__ == '__main__':
                     media.link,
                     media.created_time
                 )
-                
                 print(sql_query)
+
                 # This is where you call insert_into_cartodb()
                 insert_into_cartodb(sql_query)
-                    
-
-                # This is where you could call insert_into_cartodb()
         except exceptions.AttributeError:
             pass
